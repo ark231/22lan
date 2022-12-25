811602915405e615ae8e715ff7e025d51e110607
ark231 <arklp20909@gmail.com>
2022-12-25 18:22:53 +0900

    change: ret廃止、xchgをr3との操作に変更
    retは、endfuncに埋め込めて、途中で抜ける場合は関数の末尾にラベルをおいてそこに飛べば良いので、廃止。
    余った命令と既存のxchg系列合わせて3命令を、r012とr3のxchgに変更

--- 22lan_cxx.py
+++ 22lan_cxx.py
@@ -7,11 +7,12 @@ from lark.lark import Lark
 import base64
 import sys
 from pathlib import Path
+import re
 
 
 class Label:
     def __init__(self, lid: int):
-        self.name = f"lan22_l{lid:b}"
+        self.name = f"l{lid:b}"
         self.lid = lid
 
     def __str__(self) -> str:
@@ -44,7 +45,7 @@ class Function:
         self.labels.append(label)
 
     def add_fid(self, fid: int):
-        self.name = f"lan22_f{fid:b}"
+        self.name = f"f{fid:b}"
         self.fid = fid
 
     def __str__(self) -> str:
@@ -55,9 +56,13 @@ class Function:
         #     for label in self.labels:
         #         result += f"    ltable[{label.lid}]=&&{label.name};\n"
         for step in self.steps:
-            if step == GOTO:
+            match = re.match(f"( *){GOTO}", step)
+            if match:
+                lines = []
                 for label in self.labels:
-                    result += f"    if(r0 == {label.lid}){{goto {label.name};}}\n"
+                    lines.append(f"    {match[1]}if(r0 == {label.lid}){{goto {label.name};}}\n")
+                for line in lines:
+                    result += line
             else:
                 result += f"    {step}\n"
         result += "}"
@@ -85,14 +90,14 @@ class Stack:
         return result
 
 
-class NyulanGenerator(Transformer):
+class CPlusPlusGenerator(Transformer):
     initial_s1: list[int]
     compile_time_stack: Stack
     functions: list[Function]
 
     def __repr__(self):
         return f"""
-NyulanGenerator{{
+CPlusPlusGenerator{{
     initial_s1_size:{len(self.initial_s1)}
     initial_s1:{self.initial_s1}
 }}
@@ -123,36 +128,46 @@ NyulanGenerator{{
         if len(nodes) != 0 and nodes[0].type == "INSTRUCTION":
             instruction = nodes[0]
             if instruction == "nand":
-                self.functions[-1].add_step("r0 &= r1;")
-                self.functions[-1].add_step("r0 = !r0;")
+                self.functions[-1].add_step("r2 = r0 & r1;")
+                self.functions[-1].add_step("r2 = !r2;")
             elif instruction == "lshift":
-                self.functions[-1].add_step("if(r1 & 0xff >= 0){")
-                self.functions[-1].add_step("    r0 <<= (r1 & 0xff);")
-                self.functions[-1].add_step("}else{")
-                self.functions[-1].add_step("    r0 >>= -static_cast<std::int8_t>(r1 & 0xff);")
+                self.functions[-1].add_step("{")
+                self.functions[-1].add_step("    std::int8_t r1_8 = r1 & 0xff;")
+                self.functions[-1].add_step("    if(r1_8 >= 0){")
+                self.functions[-1].add_step("        if(r1_8 >= 64){")
+                self.functions[-1].add_step("            r2 = 0;")
+                self.functions[-1].add_step("        }else{")
+                self.functions[-1].add_step("            r2 << r0 << r1_8;")
+                self.functions[-1].add_step("        }")
+                self.functions[-1].add_step("    }else{")
+                self.functions[-1].add_step("        r1_8 = -r1_8;")
+                self.functions[-1].add_step("        if(r1_8 >= 64){")
+                self.functions[-1].add_step("            r2 = 0;")
+                self.functions[-1].add_step("        }else{")
+                self.functions[-1].add_step("            r2 = r0 >> r1_8;")
+                self.functions[-1].add_step("        }")
+                self.functions[-1].add_step("    }")
                 self.functions[-1].add_step("}")
             elif instruction == "push8s0":
-                self.functions[-1].add_step("s0.push(r0 & 0xff)")
+                self.functions[-1].add_step("s0.push(r0 & 0xff);")
             elif instruction == "pop8s0":
                 self.functions[-1].add_step("r0 ^= (r0 & 0xff);")
                 self.functions[-1].add_step("r0 |= s0.top();")
                 self.functions[-1].add_step("s0.pop();")
             elif instruction == "push8s1":
-                self.functions[-1].add_step("s1.push(r1 & 0xff)")
+                self.functions[-1].add_step("s1.push(r1 & 0xff);")
             elif instruction == "pop8s1":
                 self.functions[-1].add_step("r1 ^= (r1 & 0xff);")
                 self.functions[-1].add_step("r1 |= s1.top();")
                 self.functions[-1].add_step("s1.pop();")
             elif instruction == "push8s2":
-                self.functions[-1].add_step("s2.push(r2 & 0xff)")
+                self.functions[-1].add_step("s2.push(r2 & 0xff);")
             elif instruction == "pop8s2":
                 self.functions[-1].add_step("r2 ^= (r2 & 0xff);")
                 self.functions[-1].add_step("r2 |= s2.top();")
                 self.functions[-1].add_step("s2.pop();")
             elif instruction == "call":
                 self.functions[-1].add_step("ftable[r0]();")
-            elif instruction == "ret":
-                self.functions[-1].add_step("return;")
             elif instruction == "print":
                 self.functions[-1].add_step("std::cout.put(r0 & 0xff);")
             elif instruction == "read":
@@ -167,10 +182,6 @@ NyulanGenerator{{
                 label = Label(self.compile_time_stack.pop64())
                 self.functions[-1].add_step(f"{label};")
                 self.functions[-1].add_label(label)
-            elif instruction == "func2addr":
-                self.functions[-1].add_step("r0 = r1;")
-            elif instruction == "label2addr":
-                self.functions[-1].add_step("r0 = r1;")
             elif instruction == "zero":
                 self.compile_time_stack.push1(0)
             elif instruction == "one":
@@ -178,19 +189,21 @@ NyulanGenerator{{
             elif instruction == "ifz":
                 self.functions[-1].add_step("if(r1 == 0){")
                 # self.functions[-1].add_step("    goto *ltable[r0];")
-                self.functions[-1].add_step(GOTO)
+                self.functions[-1].add_step(f"    {GOTO}")
                 self.functions[-1].add_step("}")
             elif instruction == "pushl8":
                 self.functions[-1].add_step(f"s0.push({self.compile_time_stack.pop8()});")
-            elif instruction == "xchg01":
-                self.functions[-1].add_step("std::swap(r0,r1);")
-            elif instruction == "xchg12":
-                self.functions[-1].add_step("std::swap(r1,r2);")
+            elif instruction == "xchg03":
+                self.functions[-1].add_step("std::swap(r0,r3);")
+            elif instruction == "xchg13":
+                self.functions[-1].add_step("std::swap(r1,r3);")
+            elif instruction == "xchg23":
+                self.functions[-1].add_step("std::swap(r2,r3);")
             else:
                 raise ParseError(f"invalid instruction {instruction}", meta.line)
 
     def start(self, _):
-        lan22_initializer = Function("lan22_init")
+        lan22_initializer = Function("init")
         for func in self.functions:
             lan22_initializer.add_step(f"ftable[{func.fid}]=&{func.name};")
         for byte in reversed(self.initial_s1):
@@ -198,12 +211,13 @@ NyulanGenerator{{
         lan22_initializer.add_step("r0 = 0;")
         lan22_initializer.add_step("r1 = 0;")
         lan22_initializer.add_step("r2 = 0;")
+        lan22_initializer.add_step("r3 = 0;")
         self.functions.append(lan22_initializer)
 
         entrypoint = Function("main", "int")
         self.functions.append(entrypoint)
-        entrypoint.add_step("lan22_init();")
-        entrypoint.add_step("ftable[0]();")
+        entrypoint.add_step("lan22::init();")
+        entrypoint.add_step("lan22::ftable[0]();")
         return Discard
 
     def dump(self, outputfile) -> None:
@@ -216,7 +230,8 @@ NyulanGenerator{{
 #include<stack>
 #include<functional>
 
-std::uint64_t r0,r1,r2;
+namespace lan22{
+std::uint64_t r0,r1,r2,r3;
 std::stack<std::uint8_t> s0,s1,s2;
 std::unordered_map<std::uint64_t,std::function<void(void)>> ftable;
         """.strip(
@@ -224,8 +239,14 @@ std::unordered_map<std::uint64_t,std::function<void(void)>> ftable;
         ).rstrip(
             " "
         )
+        cxx_main = Function()
         for func in self.functions:
+            if func.name == "main":
+                cxx_main = func
+                continue
             result += f"{func}\n"
+        result += "}// namespace lan22\n"
+        result += f"{cxx_main}"
         return result
 
 
@@ -236,7 +257,7 @@ def parse(source: str) -> Tree:
 
 
 def main() -> None:
-    parser = argparse.ArgumentParser(description="convert 22lan source code to nyulan assembly", prog="22lan.py")
+    parser = argparse.ArgumentParser(description="convert 22lan source code to nyulan assembly", prog="22lan_cxx.py")
     parser.add_argument("-s", "--source", help="source file", required=True)
     parser.add_argument("-o", "--output", help="output filename (in result folder)")
     parser.add_argument("-d", "--debug", action="store_true", help="enable debug output")
@@ -247,7 +268,7 @@ def main() -> None:
 
     with open(args.source, "r", encoding="utf8") as sourcefile:
         parsed_data = parse(sourcefile.read())
-    generator = NyulanGenerator()
+    generator = CPlusPlusGenerator()
     try:
         generator.from_tree(parsed_data)
     except ParseError as e:
--- build.sh
+++ build.sh
@@ -4,6 +4,8 @@ script_dir=$(cd $(dirname $0);pwd)
 CC="g++"
 STD="gnu++20"
 
-$script_dir/22lan_cxx.py -s $1 -o $1.cpp
+result_name=$(basename $1 .22l)
 
-$CC -std=$STD -o $2 $1.cpp
+$script_dir/22lan_cxx.py -s $1 -o $result_name.cpp
+
+$CC -std=$STD -o $result_name $result_name.cpp
